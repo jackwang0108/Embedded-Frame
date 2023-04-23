79b2056d8023b8dafe9ff1d6c424e14eedacdcea
Jack <jack4shihong@gmail.com>
2023-04-23 19:41:22 +0800

    init

---
+++ main/init.c
@@ -0,0 +1,18 @@
+#include "sd.h"
+#include "rtc.h"
+#include "lcd.h"
+#include "uart.h"
+#include "shock.h"
+#include "interrupt.h"
+#include "init.h"
+
+
+void init_all(){
+    // 初始化设备
+    sd_init();
+    rtc_init();
+    lcd_init();
+    uart_init();
+    shock_init();
+    interrupt_init();
+}
